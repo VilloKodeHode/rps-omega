3372d4cecea2444dca5badc7f89393649e4e0719
Joakim Villo <villokodehode@gmail.com>
2024-03-03 10:26:20 +0100

    todo: navigation mapping

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
+import { pagesData } from "../data/pagesData";
 
 export const Navigation = () => {
+  // const currentPath = window.location.pathname;
+  // console.log(currentPath);
   return (
     <>
       <nav className="absolute z-50 grid gap-4 p-2 m-2 top-2 right-2 h-fit w-fit">
+        {/* {pagesData.map((page) => {
+          <Link key={page.path} className="h-fit w-fit" href={`/${page.path}`}>
+            <img
+              className="w-16 h-16 p-2 rounded-xl opacity-80 bg-lightBlue"
+              src={`/${page.src}`}
+            />
+          </Link>;
+        })} */}
         <Link className="h-fit w-fit" href="/RockPaperScissorsLizardSpock">
           <img
             className="w-16 h-16 p-2 rounded-xl opacity-80 bg-lightBlue"
---
+++ app/data/pagesData.js
@@ -0,0 +1,17 @@
+export const pagesData = [
+    {
+        name: "RockPaperScissors",
+        path: "/RockPaperScissors",
+        src: "/logo.svg"
+    },
+    {
+        name: "RockPaperScissorsLizardSpock",
+        path: "/RockPaperScissorsLizardSpock",
+        src: "/logo-bonus.svg"
+    },
+    {
+        name: "RockPaperScissorsLizardSpockFireWater",
+        path: "/RockPaperScissorsLizardSpockFireWater",
+        src: "/logo-firewater.png"
+    },
+]
