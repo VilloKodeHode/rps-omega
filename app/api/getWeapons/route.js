11e2a72e054de07ec6501b457520b1cf0d342ca1
Joakim Villo <villokodehode@gmail.com>
2024-03-08 18:40:14 +0100

    *

--- app/api/getWeapons/route.js
+++ app/api/getWeapons/route.js
@@ -1,48 +1,45 @@
-import { MongoClient } from "mongodb";
+import { setup } from "@/lib/mongoDB";
 
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
-  
-    const getData = await setup()
- 
-    if (getData) {
-     
-        return new NextResponse(console.log("ok"))
-    } else {
-   
-        return new NextResponse(console.log("error"))
+// export const GET = async () => {
+//     const getData = await setup()
 
-    }
+//     if (getData) {
 
-}
+//         return new NextResponse(console.log("error"))
+//     } else {
+//         return new NextResponse(console.log("ok"))
+
+//     }
+
+// }
 
 
 
+import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
-export const setup = async () => {
-
-
+export const GET = async () => {
     try {
         await client.connect();
-        console.log("connected to MongoDB");
-     
+        console.log("Connected to MongoDB");
 
-        const response = client.db("RPS").collection("weaponsInfo")
-        const data = await response.find({}).toArray()
-        // console.log("data:", data)
-        // console.log("data[0]:", data[0])
+        const response = client.db("RPS").collection("weaponsInfo");
+        const data = await response.find({}).toArray();
+        console.log("Data:", data);
+
+        // Close the MongoDB connection after using the data
         await client.close();
         console.log('Disconnected from MongoDB');
-   
-        return data
 
+        return new NextResponse(console.log("ok"));
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
+        return new NextResponse(console.log("error"));
     }
-}
+};
 
 
 
