98c7c0ff28550bb239b596485d3256315b797482
Joakim Villo <villokodehode@gmail.com>
2024-03-08 18:33:18 +0100

    *

--- app/api/getWeapons/route.js
+++ app/api/getWeapons/route.js
@@ -1,15 +1,18 @@
 import { setup } from "@/lib/mongoDB";
+import { MongoClient } from "mongodb";
 
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
+  
     const getData = await setup()
-
+ 
     if (getData) {
-
-        return new NextResponse(console.log("error"))
-    } else {
+     
         return new NextResponse(console.log("ok"))
+    } else {
+   
+        return new NextResponse(console.log("error"))
 
     }
 
@@ -17,6 +20,30 @@ export const GET = async () => {
 
 
 
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+export const setup = async () => {
+
+
+    try {
+        await client.connect();
+        console.log("connected to MongoDB");
+     
+
+        const response = client.db("RPS").collection("weaponsInfo")
+        const data = await response.find({}).toArray()
+        // console.log("data:", data)
+        // console.log("data[0]:", data[0])
+        await client.close();
+        console.log('Disconnected from MongoDB');
+   
+        return data
+
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
 
 
 
