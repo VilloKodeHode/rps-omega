5818f6dbc3f0db9620f3288b4c81850d96320bc8
Joakim-Jobloop <Joakim@jobloop.no>
2025-03-20 10:27:34 +0100

    fixed some image url errors

--- app/components/RPS/GameRules.jsx
+++ app/components/RPS/GameRules.jsx
@@ -7,8 +7,7 @@ export const RPSRules = () => {
     <div className="z-40">
       <button
         className="absolute p-2 px-6 text-white bg-transparent border-2 border-white shadow-xl hover:bg-transparent hover:border-gray-400 right-6 bottom-6 btn rounded-xl"
-        onClick={() => setToggle(!toggle)}
-      >
+        onClick={() => setToggle(!toggle)}>
         RULES
       </button>
       {toggle && (
@@ -18,12 +17,14 @@ export const RPSRules = () => {
               <h1 className="z-50 text-2xl text-left text-white">RULES</h1>
               <button
                 className="absolute z-50 p-2 bg-transparent right-3 top-3"
-                onClick={() => setToggle(!toggle)}
-              >
-                <img src="/icon-close.svg" />
+                onClick={() => setToggle(!toggle)}>
+                <img src="/images/icon-close.svg" />
               </button>
 
-              <img className="m-auto " src={`/rules-${gameType}.png`} />
+              <img
+                className="m-auto "
+                src={`/images/rules/rules-${gameType}.png`}
+              />
             </div>
           </div>
         </>
